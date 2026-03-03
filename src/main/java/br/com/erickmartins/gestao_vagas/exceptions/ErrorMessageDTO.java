765268ea0236e33bc055a3ee64051230b1192d79
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-03-03 09:42:02 -0300

    feat: implement validation for candidate request body

---
+++ src/main/java/br/com/erickmartins/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.erickmartins.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
