6f0003dd5cb103c761c91108c1408414132a7545
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-03-31 18:53:51 -0300

    feat: implement job filtering for candidates

--- src/main/java/br/com/erickmartins/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/erickmartins/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -3,7 +3,9 @@ package br.com.erickmartins.gestao_vagas.modules.candidate.controllers;
 import br.com.erickmartins.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.erickmartins.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import br.com.erickmartins.gestao_vagas.modules.candidate.services.CandidateService;
+import br.com.erickmartins.gestao_vagas.modules.candidate.services.ListAllJobsByFilterService;
 import br.com.erickmartins.gestao_vagas.modules.candidate.services.ProfileCandidateService;
+import br.com.erickmartins.gestao_vagas.modules.company.entities.JobEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,6 +25,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateService profileCandidateService;
+
+    @Autowired
+    private ListAllJobsByFilterService listAllJobsByFilterService;
 
     @PostMapping("/")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -45,5 +51,11 @@ public class CandidateController {
         } catch (Exception ex) {
             return ResponseEntity.badRequest().body(ex.getMessage());
         }
+    }
+
+    @GetMapping("/job/")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return listAllJobsByFilterService.execute(filter);
     }
 }
