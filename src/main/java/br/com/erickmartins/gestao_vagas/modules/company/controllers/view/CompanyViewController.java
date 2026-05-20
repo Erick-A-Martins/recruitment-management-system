b6528fa2fe0a396f9c7e6c6e6655461359a363d6
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-05-20 12:30:58 -0300

    feat: implement company signup

--- src/main/java/br/com/erickmartins/gestao_vagas/modules/company/controllers/view/CompanyViewController.java
+++ src/main/java/br/com/erickmartins/gestao_vagas/modules/company/controllers/view/CompanyViewController.java
@@ -1,14 +1,19 @@
 package br.com.erickmartins.gestao_vagas.modules.company.controllers.view;
 
+import br.com.erickmartins.gestao_vagas.modules.company.dto.CompanyDTO;
+import br.com.erickmartins.gestao_vagas.modules.company.services.CompanyService;
 import br.com.erickmartins.gestao_vagas.modules.job.dto.JobDTO;
 import br.com.erickmartins.gestao_vagas.modules.job.services.JobService;
 import br.com.erickmartins.gestao_vagas.security.details.CompanyDetails;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,6 +23,9 @@ public class CompanyViewController {
 
     @Autowired
     private JobService jobService;
+
+    @Autowired
+    private CompanyService companyService;
 
     @GetMapping("/jobs")
     @PreAuthorize("hasRole('COMPANY')")
@@ -35,4 +43,42 @@ public class CompanyViewController {
         jobService.createForView(jobDTO, companyDetails.getCompanyId());
         return "redirect:/company/jobs";
     }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("company", new CompanyDTO());
+        return "company/create";
+    }
+
+    @PostMapping("create")
+    public String save(
+            @Valid @ModelAttribute("company") CompanyDTO companyDTO,
+            BindingResult result,
+            Model model
+    ) {
+
+        if (result.hasErrors()) {
+            return "company/create";
+        }
+
+        if (!companyDTO.getPassword().equals(companyDTO.getConfirmPassword())) {
+            model.addAttribute("errorMessage", "As senhas não coincidem");
+            return "company/create";
+        }
+
+        try {
+            companyService.execute(companyDTO);
+        } catch (Exception ex) {
+            model.addAttribute("errorMessage", "Erro interno. Tente novamente.");
+            return "company/create";
+        }
+
+        return "redirect:company/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "company/login";
+    }
+
 }
